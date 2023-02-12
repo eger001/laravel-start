24332c89228e8aa6c9e274d19f4ccbf6b4fbf51b
eger001 <mainforphp@gmail.com>
2023-02-12 22:24:20 +0200

    show all tasks, create new task, store new task, create new row in table and fails realized

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,43 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use \Illuminate\Http\Request;
+use App\Models\Task;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+#show all tasks
+Route::get('/task', function () {
+    $tasks = Task::all();
+    return view('tasks.index', [
+        'tasks'=>$tasks,
+    ]);
+})->name('task.index');
+
+#create task form
+Route::get('/task/create', function () {
+    return view('tasks.create');
+})->name('task.create');
+
+#store new task
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name'=>'required|max:255',
+    ]);
+    #check fails during validation
+    if ($validator->fails()) {
+        return redirect()
+            ->route('task.create')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    #create new row in table tasks
+    $task = new Task();
+    $task->name =$request->name;
+    $task->save();
+
+    return redirect()->route('task.index');
+})->name('task.store');
